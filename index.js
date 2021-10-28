54150b87a01faad2f2eff30b71ac28caddf1b1ca
Tywayne <tywaynebuchanan@gmail.com>
2021-10-28 16:17:24 -0500

    chore: created container for navbar

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 document.getElementById("root").innerHTML = `
 
-<header class="header">
+<div class="container">
+    <header class="header">
 
-    <a href="#">
-        <img src="./img/logo.svg" alt="Logo" class="logo">
-    </a>
-   <div class="nav-bar">
-       <nav>
-            <ul class="nav-items">
-                <li><a class = "nav-links" href="#">Product</a></li>
-                <li><a class = "nav-links" href="#">Resources</a></li>
-                <li><a class = "nav-links" href="#">Pricing</a></li>
-                <li><a class = "nav-links" href="#">Company</a></li>
-               </ul>
-        </nav>
-
-        <nav>
-        <ul class="nav-items">
-            <li><a class ="nav-links" href="#">Contact Sales</a></li>
-            <li><a class ="nav-links" href="#">Log In</a></li>
-           
-            <li><a class="nav-links nav-cta" href="#">Start for Free</a></li>
-        </ul>   
-        </nav>
-   </div>
-     
-     
-  
+        <a href="#">
+            <img src="./img/logo.svg" alt="Logo" class="logo">
+        </a>
+       <div class="nav-bar">
+           <nav>
+                <ul class="nav-items">
+                    <li><a class = "nav-links" href="#">Product</a></li>
+                    <li><a class = "nav-links" href="#">Resources</a></li>
+                    <li><a class = "nav-links" href="#">Pricing</a></li>
+                    <li><a class = "nav-links" href="#">Company</a></li>
+                   </ul>
+            </nav>
     
-</header>
+            <nav>
+            <ul class="nav-items">
+                <li><a class ="nav-links" href="#">Contact Sales</a></li>
+                <li><a class ="nav-links" href="#">Log In</a></li>
+               
+                <li><a class="nav-links nav-cta" href="#">Start for Free</a></li>
+            </ul>   
+            </nav>
+       </div>
+         
+         
+      
+        
+    </header>
+</div>
+
 
 `;
