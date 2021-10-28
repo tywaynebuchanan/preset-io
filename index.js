f8ec0dc6480b31e107edf02f170f28acf350f78b
Tywayne <tywaynebuchanan@gmail.com>
2021-10-28 16:10:07 -0500

    created nav bar

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 document.getElementById("root").innerHTML = `
 
-<header>
-    this works;
+<header class="header">
+
+    <a href="#">
+        <img src="./img/logo.svg" alt="Logo" class="logo">
+    </a>
+   <div class="nav-bar">
+       <nav>
+            <ul class="nav-items">
+                <li><a class = "nav-links" href="#">Product</a></li>
+                <li><a class = "nav-links" href="#">Resources</a></li>
+                <li><a class = "nav-links" href="#">Pricing</a></li>
+                <li><a class = "nav-links" href="#">Company</a></li>
+               </ul>
+        </nav>
+
+        <nav>
+        <ul class="nav-items">
+            <li><a class ="nav-links" href="#">Contact Sales</a></li>
+            <li><a class ="nav-links" href="#">Log In</a></li>
+           
+            <li><a class="nav-links nav-cta" href="#">Start for Free</a></li>
+        </ul>   
+        </nav>
+   </div>
+     
+     
+  
+    
 </header>
 
 `;
